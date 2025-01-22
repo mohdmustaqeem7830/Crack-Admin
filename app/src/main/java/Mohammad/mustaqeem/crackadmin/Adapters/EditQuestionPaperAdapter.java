1268b6cfa46c8a75ea183d6e4f2b05a4c3cc87af
mohdmustaqeem7830 <mustakimraza38@gmail.com>
2025-01-22 16:33:03 +0530

    work on delete question paper

--- app/src/main/java/Mohammad/mustaqeem/crackadmin/Adapters/EditQuestionPaperAdapter.java
+++ app/src/main/java/Mohammad/mustaqeem/crackadmin/Adapters/EditQuestionPaperAdapter.java
@@ -6,16 +6,21 @@ import android.content.Intent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
 
 import java.util.ArrayList;
 
 import Mohammad.mustaqeem.crackadmin.Activites.AddQuestion;
 import Mohammad.mustaqeem.crackadmin.Activites.AddSubjectQuestion;
+import Mohammad.mustaqeem.crackadmin.Activites.addQuestionPaper;
 import Mohammad.mustaqeem.crackadmin.Model.AddQuestionPaperModel;
 import Mohammad.mustaqeem.crackadmin.Model.Question;
 import Mohammad.mustaqeem.crackadmin.databinding.QuestionListItemsBinding;
@@ -68,9 +73,9 @@ public class EditQuestionPaperAdapter extends RecyclerView.Adapter<EditQuestionP
             public void onClick(View v) {
                 dialog.show();
                 if (subject!=null){
-//                    DeleteSubjectQuestion(question.getqId(),position);
+                    DeleteSubjectQuestionPaper(questionPaperModel.getQpId(),position);
                 }else{
-//                    Deletquestion(question.getqId(),position);
+                    DeletquestionPaper(questionPaperModel.getQpId(),position);
 
                 }
             }
@@ -80,7 +85,7 @@ public class EditQuestionPaperAdapter extends RecyclerView.Adapter<EditQuestionP
             @Override
             public void onClick(View v) {
                 if(subject!=null){
-                    Intent intent = new Intent(context, AddSubjectQuestion.class);
+                    Intent intent = new Intent(context, addQuestionPaper.class);
                     intent.putExtra("categoryName",categoryName);
                     intent.putExtra("subCategoryName",subCategoryName);
                     intent.putExtra("studyCategoryName",studyCategoryName);
@@ -88,7 +93,7 @@ public class EditQuestionPaperAdapter extends RecyclerView.Adapter<EditQuestionP
                     intent.putExtra("qpId", questionPaperModel.getQpId());
                     context.startActivity(intent);
                 }else{
-                    Intent intent = new Intent(context, AddQuestion.class);
+                    Intent intent = new Intent(context, addQuestionPaper.class);
                     intent.putExtra("categoryName",categoryName);
                     intent.putExtra("subCategoryName",subCategoryName);
                     intent.putExtra("studyCategoryName",studyCategoryName);
@@ -114,4 +119,203 @@ public class EditQuestionPaperAdapter extends RecyclerView.Adapter<EditQuestionP
             binding = QuestionListItemsBinding.bind(itemView);
         }
     }
+
+
+    private void DeletquestionPaper(String qID, int position) {
+        // Retrieve the category ID
+        database.collection("categories").whereEqualTo("categoryName", categoryName).get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    if (!queryDocumentSnapshots.isEmpty()) {
+                        String catId = queryDocumentSnapshots.getDocuments().get(0).getId();
+
+                        // Retrieve the subcategory ID
+                        database.collection("categories").document(catId)
+                                .collection("subCategories").whereEqualTo("subCategoryName", subCategoryName).get()
+                                .addOnSuccessListener(queryDocumentSnapshots1 -> {
+                                    if (!queryDocumentSnapshots1.isEmpty()) {
+                                        String subCatId = queryDocumentSnapshots1.getDocuments().get(0).getId();
+
+                                        // Retrieve the subject ID
+                                        database.collection("categories").document(catId)
+                                                .collection("subCategories").document(subCatId)
+                                                .collection(studyCategoryName).document(qID).get()
+                                                                .addOnSuccessListener(documentSnapshot -> {
+                                                                    if (documentSnapshot.exists()) {
+                                                                        Question question = documentSnapshot.toObject(Question.class);
+                                                                        String imageUrl = question.getqImage();
+
+                                                                        if (imageUrl != null) {
+                                                                            // Delete image from Firebase Storage
+                                                                            StorageReference imageRef = FirebaseStorage.getInstance().getReferenceFromUrl(imageUrl);
+                                                                            imageRef.delete().addOnSuccessListener(aVoid -> {
+                                                                                // After image deletion, delete the question document
+                                                                                deleteQuestionDocument(catId, subCatId, studyCategoryName, qID, position);
+                                                                            }).addOnFailureListener(e -> {
+                                                                                dialog.dismiss();
+                                                                                Toast.makeText(context, "Failed to delete image", Toast.LENGTH_SHORT).show();
+                                                                                deleteQuestionDocument(catId, subCatId, studyCategoryName, qID, position);
+                                                                            });
+                                                                        } else {
+                                                                            // Directly delete the question document if no image URL
+                                                                            deleteQuestionDocument(catId, subCatId, studyCategoryName, qID, position);
+                                                                        }
+                                                                    } else {
+                                                                        dialog.dismiss();
+                                                                        Toast.makeText(context, "Question not found", Toast.LENGTH_SHORT).show();
+                                                                    }
+                                                                }).addOnFailureListener(e -> {
+                                                                    dialog.dismiss();
+                                                                    Toast.makeText(context, "Failed to retrieve question", Toast.LENGTH_SHORT).show();
+                                                                });
+                                                    } else {
+                                        dialog.dismiss();
+                                        Toast.makeText(context, "Subcategory not found", Toast.LENGTH_SHORT).show();
+                                    }
+                                }).addOnFailureListener(e -> {
+                                    dialog.dismiss();
+                                    Toast.makeText(context, "Failed to retrieve subcategory", Toast.LENGTH_SHORT).show();
+                                });
+                    } else {
+                        dialog.dismiss();
+                        Toast.makeText(context, "Category not found", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(e -> {
+                    dialog.dismiss();
+                    Toast.makeText(context, "Failed to retrieve category", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    // Helper function to delete the question document
+    private void deleteQuestionDocument(String catId, String subCatId, String studyCategoryName, String qID, int position) {
+        database.collection("categories").document(catId)
+                .collection("subCategories").document(subCatId)
+                .collection(studyCategoryName).document(qID).delete()
+                .addOnSuccessListener(aVoid -> {
+                    dialog.dismiss();
+                    questionpaperArrayList.remove(position);
+                    notifyDataSetChanged();
+                    Toast.makeText(context, "Question Paper deleted successfully", Toast.LENGTH_SHORT).show();
+                    // Optionally, update UI or notify user
+                }).addOnFailureListener(e -> {
+                    dialog.dismiss();
+                    Toast.makeText(context, "Failed to delete question", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    private void DeleteSubjectQuestion(String qID, int position) {
+
+        database.collection("categories").whereEqualTo("categoryName", categoryName).get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    if (queryDocumentSnapshots.isEmpty()) {
+                        dialog.dismiss();
+                        Toast.makeText(context, "No categories found", Toast.LENGTH_SHORT).show();
+                        return;
+                    }
+                    String catId = queryDocumentSnapshots.getDocuments().get(0).getId();
+
+                    // Fetch sub-categories
+                    database.collection("categories").document(catId).collection("subCategories")
+                            .whereEqualTo("subCategoryName", subCategoryName).get()
+                            .addOnSuccessListener(queryDocumentSnapshots1 -> {
+                                if (queryDocumentSnapshots1.isEmpty()) {
+                                    dialog.dismiss();
+                                    Toast.makeText(context, "No sub-categories found", Toast.LENGTH_SHORT).show();
+                                    return;
+                                }
+                                String subcatId = queryDocumentSnapshots1.getDocuments().get(0).getId();
+
+                                // Fetch question papers
+                                database.collection("categories").document(catId)
+                                        .collection("subCategories").document(subcatId)
+                                        .collection(studyCategoryName).whereEqualTo("subjectName", subject).get()
+                                        .addOnSuccessListener(queryDocumentSnapshots2 -> {
+                                            if (queryDocumentSnapshots2.isEmpty()) {
+                                                dialog.dismiss();
+                                                Toast.makeText(context, "No question papers found", Toast.LENGTH_SHORT).show();
+                                                return;
+                                            }
+                                            String subjectId = queryDocumentSnapshots2.getDocuments().get(0).getId();
+
+                                            // Fetch specific question paper
+                                            database.collection("categories").document(catId)
+                                                    .collection("subCategories").document(subcatId)
+                                                    .collection(studyCategoryName).document(subjectId)
+                                                    .collection("subject_question_paper").whereEqualTo("qpName", qpname).get()
+                                                    .addOnSuccessListener(queryDocumentSnapshots3 -> {
+                                                        if (queryDocumentSnapshots3.isEmpty()) {
+                                                            dialog.dismiss();
+                                                            Toast.makeText(context, "No question papers found", Toast.LENGTH_SHORT).show();
+                                                            return;
+                                                        }
+                                                        String qpID = queryDocumentSnapshots3.getDocuments().get(0).getId();
+                                                        DocumentReference qpDocRef = database.collection("categories").document(catId)
+                                                                .collection("subCategories").document(subcatId)
+                                                                .collection(studyCategoryName).document(subjectId)
+                                                                .collection("subject_question_paper").document(qpID);
+
+                                                        // Fetch the question document
+                                                        qpDocRef.collection("questions").document(qID).get().addOnSuccessListener(documentSnapshot -> {
+                                                            if (documentSnapshot.exists()) {
+                                                                Question question = documentSnapshot.toObject(Question.class);
+                                                                String imageUrl =  question.getqImage();
+
+                                                                if (imageUrl != null) {
+                                                                    // Delete image from Firebase Storage
+                                                                    StorageReference imageRef = FirebaseStorage.getInstance().getReferenceFromUrl(imageUrl);
+                                                                    imageRef.delete().addOnSuccessListener(aVoid -> {
+                                                                        // After image deletion, delete the question document
+                                                                        deleteQuestionDocument(qpDocRef, qID, position);
+                                                                    }).addOnFailureListener(e -> {
+                                                                        dialog.dismiss();
+                                                                        Toast.makeText(context, "Failed to delete image", Toast.LENGTH_SHORT).show();
+                                                                        deleteQuestionDocument(qpDocRef, qID, position);
+                                                                    });
+                                                                } else {
+                                                                    deleteQuestionDocument(qpDocRef, qID, position);
+                                                                }
+                                                            } else {
+                                                                dialog.dismiss();
+                                                                Toast.makeText(context, "Question not found", Toast.LENGTH_SHORT).show();
+                                                            }
+                                                        }).addOnFailureListener(e -> {
+                                                            dialog.dismiss();
+                                                            Toast.makeText(context, "Failed to fetch question details", Toast.LENGTH_SHORT).show();
+                                                        });
+
+                                                    })
+                                                    .addOnFailureListener(e -> {
+                                                        dialog.dismiss();
+                                                        Toast.makeText(context, "Failed to fetch question papers", Toast.LENGTH_SHORT).show();
+                                                    });
+                                        })
+                                        .addOnFailureListener(e -> {
+                                            dialog.dismiss();
+                                            Toast.makeText(context, "Failed to fetch study categories", Toast.LENGTH_SHORT).show();
+                                        });
+                            })
+                            .addOnFailureListener(e -> {
+                                dialog.dismiss();
+                                Toast.makeText(context, "Failed to fetch sub-categories", Toast.LENGTH_SHORT).show();
+                            });
+                })
+                .addOnFailureListener(e -> {
+                    dialog.dismiss();
+                    Toast.makeText(context, "Failed to fetch categories", Toast.LENGTH_SHORT).show();
+                });
+    }
+
+    // Method to delete the question document and update the RecyclerView
+    private void deleteQuestionDocument(DocumentReference qpDocRef, String qID, int position) {
+        qpDocRef.collection("questions").document(qID).delete().addOnSuccessListener(unused -> {
+            dialog.dismiss();
+            Toast.makeText(context, "Deleted Successfully", Toast.LENGTH_SHORT).show();
+
+            questionpaperArrayList.remove(position);
+            notifyDataSetChanged();
+        }).addOnFailureListener(e -> {
+            dialog.dismiss();
+            Toast.makeText(context, "Failed to delete question", Toast.LENGTH_SHORT).show();
+        });
+    }
+
 }
