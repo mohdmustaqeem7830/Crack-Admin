17790a1bb052e6e46653bd8e2b31bb1e03f4f101
mohdmustaqeem7830 <mustakimraza38@gmail.com>
2025-01-27 15:38:11 +0530

    fine

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -51,5 +51,5 @@ dependencies {
     implementation("com.google.firebase:firebase-auth")
     implementation("com.google.firebase:firebase-storage")
     implementation("com.google.firebase:firebase-firestore")
-    implementation("com.github.CanHub:ImageCropper:4.4.0")
+
 }
